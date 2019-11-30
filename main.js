bae5c093a0367c3847520a0830afd88453aba819
Wahaj Aayani <wahaj_aayani@hotmail.com>
2019-12-01 02:23:27 +0500

    refactoring

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import 'core-js/features/array/flat-map';
 
 import cors from 'cors';
 import express from 'express';
-import graph from 'src/algorithms/dijkstra';
+import Graph from 'src/algorithms/dijkstra';
+import { calculateViscosity } from 'src/utils';
 
 import blocksData from 'data/blocks.json';
 import transports from 'data/transports.json';
@@ -11,40 +12,11 @@ const app = express();
 app.disable('x-powered-by');
 app.use(cors());
 
-const calculateViscosity = (lanes, carCount, busAllowed = true) => {
-  let viscosity = 0;
-
-  if (lanes === 1) {
-    viscosity += 4;
-  } else if (lanes > 1) {
-    viscosity += 3;
-  } else if (lanes > 2) {
-    viscosity += 2;
-  } else if (lanes > 3) {
-    viscosity += 0;
-  }
-
-  if (carCount >= 0 && carCount < 1) {
-    viscosity += 1;
-  } else if (carCount >= 1 && carCount < 2) {
-    viscosity += 2;
-  } else if (carCount >= 2 && carCount < 3) {
-    viscosity += 3;
-  } else {
-    viscosity += 4;
-  }
-
-  if (!busAllowed) {
-    viscosity += transports[0][2];
-  }
-
-  return viscosity;
-};
-
 const blocks = blocksData.map(v1 =>
   v1.map(v2 => ({
     ...v2,
     viscosity: calculateViscosity(
+      transports,
       v2.stats.lanes,
       v2.stats.carCount,
       v2.stats.busAllowed
@@ -52,36 +24,36 @@ const blocks = blocksData.map(v1 =>
   }))
 );
 
-const g = new graph();
+const graph = new Graph();
 
-g.addVertex('A', { B: 1, F: 2 });
-g.addVertex('B', { G: 3, C: 1 });
-g.addVertex('C', { H: 5, D: 9 });
-g.addVertex('D', { I: 8, E: 3 });
-g.addVertex('E', { I: 2 });
-g.addVertex('F', { G: 1, K: 4 });
-g.addVertex('G', { H: 6, L: 5 });
-g.addVertex('H', { I: 7, M: 9 });
-g.addVertex('I', { J: 4, N: 2 });
-g.addVertex('J', { O: 5 });
-g.addVertex('K', { L: 2 }, { P: 5 });
-g.addVertex('L', { Q: 7 }, { M: 3 });
-g.addVertex('M', { N: 5 }, { R: 9 });
-g.addVertex('N', { S: 1 }, { O: 8 });
-g.addVertex('O', { T: 8 });
-g.addVertex('P', { Q: 1 }, { U: 5 });
-g.addVertex('Q', { R: 6 }, { V: 2 });
-g.addVertex('R', { S: 4 }, { W: 3 });
-g.addVertex('S', { T: 4 }, { X: 2 });
-g.addVertex('T', { Y: 2 });
-g.addVertex('U', {});
-g.addVertex('V', {});
-g.addVertex('W', {});
-g.addVertex('X', {});
-g.addVertex('Y', {});
+graph.addVertex('A', { B: 1, F: 2 });
+graph.addVertex('B', { G: 3, C: 1 });
+graph.addVertex('C', { H: 5, D: 9 });
+graph.addVertex('D', { I: 8, E: 3 });
+graph.addVertex('E', { I: 2 });
+graph.addVertex('F', { G: 1, K: 4 });
+graph.addVertex('G', { H: 6, L: 5 });
+graph.addVertex('H', { I: 7, M: 9 });
+graph.addVertex('I', { J: 4, N: 2 });
+graph.addVertex('J', { O: 5 });
+graph.addVertex('K', { L: 2 }, { P: 5 });
+graph.addVertex('L', { Q: 7 }, { M: 3 });
+graph.addVertex('M', { N: 5 }, { R: 9 });
+graph.addVertex('N', { S: 1 }, { O: 8 });
+graph.addVertex('O', { T: 8 });
+graph.addVertex('P', { Q: 1 }, { U: 5 });
+graph.addVertex('Q', { R: 6 }, { V: 2 });
+graph.addVertex('R', { S: 4 }, { W: 3 });
+graph.addVertex('S', { T: 4 }, { X: 2 });
+graph.addVertex('T', { Y: 2 });
+graph.addVertex('U', {});
+graph.addVertex('V', {});
+graph.addVertex('W', {});
+graph.addVertex('X', {});
+graph.addVertex('Y', {});
 
 const getPaths = (start, end) => {
-  const paths = g.shortestPath(start, end).map(name => {
+  const paths = graph.shortestPath(start, end).map(name => {
     const block = blocks.flatMap(b => b).find(b => b.name === name);
     let mode;
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,38 @@ export const getQuadraticDistance = (x1, y1, x2, y2) =>
 
 export const getRandomNumber = (min = 0, max = 10) =>
   Math.random() * (max - min) + min;
+
+export const calculateViscosity = (
+  transports,
+  lanes,
+  carCount,
+  busAllowed = true
+) => {
+  let viscosity = 0;
+
+  if (lanes === 1) {
+    viscosity += 4;
+  } else if (lanes > 1) {
+    viscosity += 3;
+  } else if (lanes > 2) {
+    viscosity += 2;
+  } else if (lanes > 3) {
+    viscosity += 0;
+  }
+
+  if (carCount >= 0 && carCount < 1) {
+    viscosity += 1;
+  } else if (carCount >= 1 && carCount < 2) {
+    viscosity += 2;
+  } else if (carCount >= 2 && carCount < 3) {
+    viscosity += 3;
+  } else {
+    viscosity += 4;
+  }
+
+  if (!busAllowed) {
+    viscosity += transports[0][2];
+  }
+
+  return viscosity;
+};
